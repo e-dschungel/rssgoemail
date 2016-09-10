2473913688e01d2e831d6254028c45dc85366af7
e-dschungel <github@e-dschungel.de>
2016-09-10 10:11:53 +0200

    fixed config path

--- rssgoemail.php
+++ rssgoemail.php
@@ -19,7 +19,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-	require_once(dirname(__FILE__).'/conf/config.php');
+	require_once(dirname(__FILE__).'/config/config.php');
 	require_once(dirname(__FILE__).'/lib/mail_utf8.php');
 	require_once(dirname(__FILE__).'/vendor/autoload.php');
 
