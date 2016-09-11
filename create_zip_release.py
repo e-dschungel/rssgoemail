cb9372252d70a532f0320189a9da3a8aaeaf420e
e-dschungel <github@e-dschungel.de>
2016-09-11 20:30:12 +0200

    added script to create zip release

---
+++ create_zip_release.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#script to create a ZIP release file
+
+import subprocess
+import tempfile
+import shutil
+import os
+
+# config
+COMPOSER = "composer.phar"
+REPO_URL = "https://github.com/e-dschungel/rssgoemail.git"
+ZIP_FILE_NAME = "rssgoemail.zip"
+EXCLUDE_PATTERN = ["*.git/*", "create_zip_release.py", ".gitignore"]
+
+# create ZIP
+cwd = os.getcwd()
+with tempfile.TemporaryDirectory() as tempdir:
+    os.chdir(tempdir)
+    subprocess.call(["git", "clone", REPO_URL, tempdir])
+    subprocess.call([COMPOSER, "install"])
+    subprocess.call(["zip", "-r", ZIP_FILE_NAME, ".",
+                     "-x"] + EXCLUDE_PATTERN)
+    abs_zip_path = os.path.join(cwd,ZIP_FILE_NAME)
+    if os.path.isfile(abs_zip_path):
+        os.remove(abs_zip_path)
+    shutil.move(ZIP_FILE_NAME, abs_zip_path)
+
+print("Created ZIP release file {}".format(ZIP_FILE_NAME))
