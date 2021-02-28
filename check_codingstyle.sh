faf9c79e70f5256ffe914f61c701f0aa8b979b64
e-dschungel <github@e-dschungel.de>
2021-02-28 20:04:36 +0100

    - check compatibilty for MIN_PHP_VERSION and above - added commented line to check for 3rd party libs

--- check_codingstyle.sh
+++ check_codingstyle.sh
@@ -2,8 +2,8 @@
 
 PHPCS_EXEC="./vendor/bin/phpcs"
 PHPCS_IGNORE_PATHS="*/vendor/*"
+PHPCOMPATIBILITY_IGNORE_PATHS="*/vendor/squizlabs/*,*/vendor/PHPCompatibility/*"
 MIN_PHP_VERSION="5.6"
-#PEAR_EXCLUDED_RULES="PEAR.WhiteSpace.ScopeIndent,Generic.Files.LineLength"
 
 echo "Check PHP Coding Standard PSR12"
 $PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS --standard=psr12 .
@@ -12,4 +12,5 @@ echo "Check PHP Coding Standard PEAR (modified)"
 $PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS -s --standard=mypear.xml .
 
 echo "Check PHP Compatibility > $MIN_PHP_VERSION"
-$PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS --standard=PHPCompatibility --runtime-set testVersion $MIN_PHP_VERSION .
+$PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS --standard=PHPCompatibility --runtime-set testVersion ${MIN_PHP_VERSION}- .
+#$PHPCS_EXEC --ignore=$PHPCOMPATIBILITY_IGNORE_PATHS --standard=PHPCompatibility --runtime-set testVersion ${MIN_PHP_VERSION}- .
