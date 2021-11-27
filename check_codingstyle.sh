4d4c4ba7eaf3019ddf3b200ce745a380dadea001
e-dschungel <github@e-dschungel.de>
2021-11-27 10:45:21 +0100

    fixed output to php version check to >=

--- check_codingstyle.sh
+++ check_codingstyle.sh
@@ -11,6 +11,6 @@ $PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS --standard=psr12 .
 echo "Check PHP Coding Standard PEAR (modified)"
 $PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS -s --standard=mypear.xml .
 
-echo "Check PHP Compatibility > $MIN_PHP_VERSION"
+echo "Check PHP Compatibility >= $MIN_PHP_VERSION"
 $PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS --standard=PHPCompatibility --runtime-set testVersion ${MIN_PHP_VERSION}- .
 #$PHPCS_EXEC --ignore=$PHPCOMPATIBILITY_IGNORE_PATHS --standard=PHPCompatibility --runtime-set testVersion ${MIN_PHP_VERSION}- .
