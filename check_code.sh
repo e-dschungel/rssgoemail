90ead6ba95af3957d6a1899cb01fa47bcd49c42a
e-dschungel <github@e-dschungel.de>
2023-01-29 09:53:37 +0100

    - check for PHP 7.2 compliance (due to SimplePie 1.8.0)

--- check_code.sh
+++ check_code.sh
@@ -3,7 +3,7 @@
 PHPCS_EXEC="./vendor/bin/phpcs"
 PHPCS_IGNORE_PATHS="*/vendor/*,*/.phpdoc/*"
 PHPCOMPATIBILITY_IGNORE_PATHS="*/vendor/squizlabs/*,*/vendor/PHPCompatibility/*"
-MIN_PHP_VERSION="5.6"
+MIN_PHP_VERSION="7.2"
 PHPSTAN_EXEC="./vendor/bin/phpstan"
 PHPSTAN_FILES_AND_DIRS="rssgoemail.php src"
 PHPSTAN_LEVEL="1"
